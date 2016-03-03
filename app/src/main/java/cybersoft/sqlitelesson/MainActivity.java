b9a4fda01a4b1868850c3299fc285cfe3ec870c0
Yorksik <Yorksik@yandex.ru>
2016-03-04 00:19:17 +0300

    2

--- app/src/main/java/cybersoft/sqlitelesson/MainActivity.java
+++ app/src/main/java/cybersoft/sqlitelesson/MainActivity.java
@@ -17,7 +17,7 @@ import java.security.PublicKey;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public static final String TAG = "Log";
+    public static final String TAG = "Log_d";
 
     private dbHelper mDatabaseHelper;
     private SQLiteDatabase mSqLiteDatabase;
@@ -41,15 +41,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         EditText Email =(EditText)findViewById(R.id.editEmail);
         EditText Name =(EditText)findViewById(R.id.editName);
 
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        btnRead = (Button)findViewById(R.id.btnRead);
+        btnClear = (Button)findViewById(R.id.btnClear);
+
         //так как в классе dbHelp описаны два (три ?) конструктора мы можем воспользоваться одним из них
         //mDatabaseHelper = new dbHelper(this, "mydatabase.db", null, 1);
         mDatabaseHelper = new dbHelper(this);
         SQLiteDatabase sdb;
         sdb = mDatabaseHelper.getReadableDatabase();
-        Toast.makeText(getApplicationContext(),"База данных создано только что",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "База данных создано только что", Toast.LENGTH_SHORT).show();
 
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues values = new ContentValues();
+                // Задайте значения для каждого столбца
+                String name = findViewById(R.id.editName).toString();
+                values.put(dbHelper.DB_TEBLE_NAME_COLUMN_NAME, name);
+
+                String Email = findViewById(R.id.editEmail).toString();
+                values.put(dbHelper.DB_TEBLE_NAME_COLUMN_EMAIL, Email);
+
+                // Вставляем данные в таблицу
+                mSqLiteDatabase.insert("cats", null, values);
+                //Toast.makeText(this, "Данные записаны в таблицу!" + name + Email, Toast.LENGTH_SHORT).show();
+                Log.d(TAG,"Данные записаны в таблицу!" + name + Email);
+            }
+        });
 
     }
+
 
 
     @Override
@@ -81,9 +103,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                             do {
                                 Log.d(TAG,
-                                        "ID "+ cursor.getInt(idColInd)+
-                                        ", NAME "+ cursor.getString(nameColInd)+
-                                        ", EMAIl "+cursor.getString(emailColInd));
+                                        "ID " + cursor.getInt(idColInd) +
+                                                ", NAME " + cursor.getString(nameColInd) +
+                                                ", EMAIl " + cursor.getString(emailColInd));
 
                             } while (cursor.moveToNext());
 
