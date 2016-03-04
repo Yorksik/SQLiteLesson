21c928594418365f8037cbe5585dd8368f87c7f7
Yorksik <Yorksik@yandex.ru>
2016-03-04 14:22:51 +0300

    3

--- app/src/main/java/cybersoft/sqlitelesson/MainActivity.java
+++ app/src/main/java/cybersoft/sqlitelesson/MainActivity.java
@@ -23,106 +23,91 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private SQLiteDatabase mSqLiteDatabase;
 
 
-
     Button btnAdd;
     Button btnClear;
     Button btnRead;
     Cursor cursor;
+    EditText Email;
+    EditText Name;
     //SQLiteOpenHelper db;
     SQLiteDatabase db;
-
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText Email =(EditText)findViewById(R.id.editEmail);
-        EditText Name =(EditText)findViewById(R.id.editName);
+        Email = (EditText) findViewById(R.id.editEmail);
+        Name = (EditText) findViewById(R.id.editName);
 
-        btnAdd = (Button)findViewById(R.id.btnAdd);
-        btnRead = (Button)findViewById(R.id.btnRead);
-        btnClear = (Button)findViewById(R.id.btnClear);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnRead = (Button) findViewById(R.id.btnRead);
+        btnClear = (Button) findViewById(R.id.btnClear);
 
-        //так как в классе dbHelp описаны два (три ?) конструктора мы можем воспользоваться одним из них
-        //mDatabaseHelper = new dbHelper(this, "mydatabase.db", null, 1);
-        mDatabaseHelper = new dbHelper(this);
-        SQLiteDatabase sdb;
-        sdb = mDatabaseHelper.getReadableDatabase();
-        Toast.makeText(getApplicationContext(), "База данных создано только что", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "Кнопки определены");
 
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ContentValues values = new ContentValues();
-                // Задайте значения для каждого столбца
-                String name = findViewById(R.id.editName).toString();
-                values.put(dbHelper.DB_TEBLE_NAME_COLUMN_NAME, name);
 
-                String Email = findViewById(R.id.editEmail).toString();
-                values.put(dbHelper.DB_TEBLE_NAME_COLUMN_EMAIL, Email);
-
-                // Вставляем данные в таблицу
-                mSqLiteDatabase.insert("cats", null, values);
-                //Toast.makeText(this, "Данные записаны в таблицу!" + name + Email, Toast.LENGTH_SHORT).show();
-                Log.d(TAG,"Данные записаны в таблицу!" + name + Email);
-            }
-        });
 
     }
 
 
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()){
+    public void onClick(View v) {
+        Log.d(TAG, "Зашли в онклик");
+        switch (v.getId()) {
             case R.id.btnAdd:
-                ContentValues values = new ContentValues();
-                // Задайте значения для каждого столбца
-                String name = findViewById(R.id.editName).toString();
-                values.put(dbHelper.DB_TEBLE_NAME_COLUMN_NAME, name);
+                        Log.d(TAG, "Нажата кнопка ADD");
+        //так как в классе dbHelp описаны два (три ?) конструктора мы можем воспользоваться одним из них
+        //mDatabaseHelper = new dbHelper(this, "mydatabase.db", null, 1);
+        mDatabaseHelper = new dbHelper(this);
+        mSqLiteDatabase = mDatabaseHelper.getWritableDatabase();
+        Toast.makeText(getApplicationContext(), "База данных создано только что", Toast.LENGTH_SHORT).show();
 
-                String Email = findViewById(R.id.editEmail).toString();
-                values.put(dbHelper.DB_TEBLE_NAME_COLUMN_EMAIL, Email);
+        ContentValues values = new ContentValues();
+        // Задайте значения для каждого столбца
+        String name = findViewById(R.id.editName).toString();
+        values.put(dbHelper.DB_TEBLE_NAME_COLUMN_NAME, name);
 
-                // Вставляем данные в таблицу
-                mSqLiteDatabase.insert("cats", null, values);
-                Toast.makeText(this, "Данные записаны в таблицу!" + name + Email, Toast.LENGTH_SHORT).show();
+        String Email = findViewById(R.id.editEmail).toString();
+        values.put(dbHelper.DB_TEBLE_NAME_COLUMN_EMAIL, Email);
+
+        // Вставляем данные в таблицу
+        mSqLiteDatabase.insert("Tebel_Name", null, values);
+        Toast.makeText(this, "Данные записаны в таблицу!" + name + Email, Toast.LENGTH_LONG).show();
+        Log.d(TAG, "Нажата кнопка ADD" + name + Email);
                 break;
-                    case R.id.btnRead:
-                        cursor = db.query(dbHelper.DB_NAME, null, null, null, null, null, null);
+            case R.id.btnRead:
+                Log.d(TAG, "Зашли в Read");
+                cursor = db.query(dbHelper.DB_NAME, null, null, null, null, null, null);
 
 
-                        if (cursor.moveToFirst()) {
+                if (cursor.moveToFirst()) {
 
-                            int idColInd = cursor.getColumnIndex(dbHelper._ID);
-                            int nameColInd = cursor.getColumnIndex(dbHelper.DB_TEBLE_NAME_COLUMN_NAME);
-                            int emailColInd = cursor.getColumnIndex(dbHelper.DB_TEBLE_NAME_COLUMN_EMAIL);
+                    int idColInd = cursor.getColumnIndex(dbHelper._ID);
+                    int nameColInd = cursor.getColumnIndex(dbHelper.DB_TEBLE_NAME_COLUMN_NAME);
+                    int emailColInd = cursor.getColumnIndex(dbHelper.DB_TEBLE_NAME_COLUMN_EMAIL);
 
 
-                            do {
-                                Log.d(TAG,
-                                        "ID " + cursor.getInt(idColInd) +
-                                                ", NAME " + cursor.getString(nameColInd) +
-                                                ", EMAIl " + cursor.getString(emailColInd));
+                    do {
+                        Log.d(TAG,
+                                "ID " + cursor.getInt(idColInd) +
+                                        ", NAME " + cursor.getString(nameColInd) +
+                                        ", EMAIl " + cursor.getString(emailColInd));
 
-                            } while (cursor.moveToNext());
+                    } while (cursor.moveToNext());
 
-                        } else {
-                            Log.d(TAG, "В базе нет данных!");
-                        }
+                } else {
+                    Log.d(TAG, "В базе нет данных!");
+                }
 
-                        cursor.close();
-
+                cursor.close();
 
 
                 break;
-                    case R.id.btnClear:
+            case R.id.btnClear:
 
                 break;
         }
-
     }
-
 }
